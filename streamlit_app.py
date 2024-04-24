52d0d228eb58b615ac07f079beb632963e441d8f
Christian Seville <seville2020christian@gmail.com>
2024-04-25 04:44:59 +0800

    Test streamlit UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,27 +22,27 @@ model = pickle.load(open('titanic_classification.pkl', 'rb'))
 st.title("Titanic Survival Classification")
 
 # text input where user will enter the SMS text to predict (As shown above)
-input_sms = st.text_area("Enter the message: ")
+input_p_class = st.text_area("Enter passenger class: ")
 
 # predict button, when clicked will execute the process
 if st.button('Predict'):
 
     # 1. preprocess - converting the input_sms received by user on app
-    transform_sms = input_sms
-    print(type(transform_sms))
-    transform_sms = np.array(transform_sms)  # converting the list of string format to array of string format
-
-    # 2. vectorize - converting the received text SMS into numeric for model understanding
-    vector_input = tfidf.transform(transform_sms.astype('str')).toarray()
-    print(type(vector_input))
-    print(vector_input)
-    vector_input = pd.DataFrame(vector_input, columns=tfidf.get_feature_names_out())
-
-    # 3. predict - passing the converted text to model to predict if it is spam or ham
-    prediction = model.predict(vector_input)[0]
-
-    # 4. display - the result on app itself , if prediction result is 1 then ui(button) will display  Spam else Not Spam
-    if prediction == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+    transform_sms = input_p_class
+    # print(type(transform_sms))
+    # transform_sms = np.array(transform_sms)  # converting the list of string format to array of string format
+    #
+    # # 2. vectorize - converting the received text SMS into numeric for model understanding
+    # vector_input = tfidf.transform(transform_sms.astype('str')).toarray()
+    # print(type(vector_input))
+    # print(vector_input)
+    # vector_input = pd.DataFrame(vector_input, columns=tfidf.get_feature_names_out())
+    #
+    # # 3. predict - passing the converted text to model to predict if it is spam or ham
+    # prediction = model.predict(vector_input)[0]
+    #
+    # # 4. display - the result on app itself , if prediction result is 1 then ui(button) will display  Spam else Not Spam
+    # if prediction == 1:
+    #     st.header("Spam")
+    # else:
+    #     st.header("Not Spam")
