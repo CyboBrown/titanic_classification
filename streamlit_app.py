f3beff1516106f1627b20357fcb6e35949d25426
Christian Seville <seville2020christian@gmail.com>
2024-04-25 06:45:38 +0800

    UI Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,41 +24,37 @@ st.title("Titanic Survival Classification")
 # text input where user will enter the parameters
 input_p_id = float(st.number_input("Passenger ID: ", 0, 10000, step=1))
 input_p_class = st.selectbox("Passenger Class: ", ["1st", "2nd", "3rd"])
-input_sex = st.selectbox("Gender: ", ["Male", "Female"])
+input_sex = st.selectbox("Sex: ", ["Male", "Female"])
 input_age = float(st.number_input("Age: ", 0, 200, step=1))
 input_sib_sp = float(st.number_input("No. of Siblings/Spouse: ", 0, 100, step=1))
 input_par_ch = float(st.number_input("No. of Parents/Children: ", 0, 100, step=1))
 input_fare = float(st.number_input("Fare: "))
 input_embarked = st.selectbox("Port of Embarkation: ", ["Cherbourg", "Queenstown", "Southampton"])
 input_title = st.selectbox("Title: ", ['Mr', 'Miss', 'Mrs', 'Master', 'Others'])
-# input_is_mr
-# input_age_group
-# input_is_elderly
-# input_family_size
-# input_is_alone
-# input_fare_group
+
 
 # predict button, when clicked will execute the process
 if st.button('Predict'):
+    st.header([input_p_id, input_p_class, input_sex, input_age, input_sib_sp, input_par_ch, input_fare, input_embarked, input_title])
+    input_p_class = 1 if (input_p_class == '1st') else 2 if (input_p_class == '2nd') else 3 if (input_p_class == '3rd') else 0
+    input_sex = 0 if (input_sex == 'Male') else 1
+    input_embarked = 0 if (input_p_class == 'Southampton') else 1 if (input_p_class == 'Cherbourg') else 2 if (input_p_class == 'Queenstown') else -1
+    titles = ['Mr', 'Miss', 'Mrs', 'Master', 'Others']
+    input_title = titles.index(input_title)
+    # input_is_mr
+    # input_age_group
+    # input_is_elderly
+    # input_family_size
+    # input_is_alone
+    # input_fare_group
 
-    # 1. preprocess - converting the input_sms received by user on app
-    transform_sms = input_p_class
-    print(type(transform_sms))
-    transform_sms = np.array(transform_sms)  # converting the list of string format to array of string format
-
-    # # 2. vectorize - converting the received text SMS into numeric for model understanding
-    # vector_input = tfidf.transform(transform_sms.astype('str')).toarray()
-    # print(type(vector_input))
-    # print(vector_input)
-    # vector_input = pd.DataFrame(vector_input, columns=tfidf.get_feature_names_out())
-
-    # 3. predict - passing the converted text to model to predict if it is spam or ham
-    vector_input = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    prediction = model.predict(vector_input)[0]
-    st.header(prediction)
-
-    # 4. display - the result on app itself , if prediction result is 1 then ui(button) will display  Spam else Not Spam
-    # if prediction == 1:
-    #     st.header("Spam")
+    # vector_input = [
+    #     [input_p_id, input_p_class, input_sex, input_age, input_sib_sp, input_par_ch, input_fare, input_embarked, input_title,
+    #      input_is_mr, input_age_group, input_is_elderly, input_family_size, input_is_alone, input_fare_group]
+    # ]
+    # prediction = model.predict(vector_input)[0][0]
+    #
+    # if prediction >= 0.5:
+    #     st.header("Survive")
     # else:
-    #     st.header("Not Spam")
+    #     st.header("Die")
